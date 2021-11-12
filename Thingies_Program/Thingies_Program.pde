36695006f9b4a44edf3e16837727d358018a0f04
Vivian Mak <67982651+VivianMak@users.noreply.github.com>
2021-11-12 09:07:16 -0800

    Add files via upload

---
+++ Thingies_Program/Thingies_Program.pde
@@ -0,0 +1,10 @@
+public void setup()
+{
+  Thingy bob = new Thingy();
+  Thingy mary = new Thingy();
+  bob.setmyX(30);
+  bob.setmyY(30);
+  mary.setmyX(70);
+  mary.setmyY(70);
+  System.out.println("Distance between bob and mary is " + dist(bob.getmyX(), bob.getmyY(), mary.getmyX(), mary.getmyY()));
+}
---
+++ Thingies_Program/Thingy.java
@@ -0,0 +1,23 @@
+class Thingy
+{
+  private int myX, myY;
+  public Thingy()
+  {
+    myX = myY = 0;
+  }
+  /* your java code for "getters" and "setters" here */
+  
+  public int getmyX(){
+    return myX;
+  }
+  public void setmyX(int x){
+    myX = x;
+  }
+  
+  public int getmyY(){
+    return myY;
+  }
+  public void setmyY(int y){
+    myY = y;
+  }
+}
